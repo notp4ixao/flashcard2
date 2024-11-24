a7ef47c5512a522948a1eb69bed35ab08dc832e2
Mickaelly kauanne <00001102212313sp@al.educacao.sp.gov.br>
2024-11-24 16:35:50 -0300

    finalizado

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é o VSCode?',
+    'O VSCode (Visual Studio Code) é um editor de código-fonte leve e poderoso, utilizado para programar em diversas linguagens, com suporte a extensões e recursos como depuração, controle de versão e autocompletar.'
+)
+
+criaCartao(
+    'Geografia',
+    ' O que é a linha do Equador?',
+    ' A linha do Equador é uma linha imaginária que divide a Terra em dois hemisférios: o hemisfério norte e o hemisfério sul.'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é ideologia?',
+    ' Ideologia é um conjunto de ideias, valores e crenças que orientam a visão de mundo e as ações de um grupo social, político ou econômic'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
